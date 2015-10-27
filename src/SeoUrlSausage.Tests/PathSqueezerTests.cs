595e8afc2b0b0b4d4862301b6673e78c3347ccc5
Chris S. <mrshrinkray@gmail.com>
2015-10-27 00:28:58 +0000

    Hindi fun

---
+++ src/SeoUrlSausage.Tests/PathSqueezerTests.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace SeoUrlSausage.Tests
+{
+	public class PathSqueezerTests
+	{
+		[Test]
+		public void should_remove_non_letters_and_digits()
+		{
+			// Arrange
+			var urlSqueezer = new PathSqueezer();
+			string title = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789-._~:/?#[]@!$&'()*+,;=";
+
+			// Act
+			string actualPath = urlSqueezer.Squeeze(title);
+
+			// Assert
+			string expectedPath = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+
+			Assert.That(actualPath, Is.EqualTo(expectedPath));
+		}
+
+		[Test]
+		public void should_not_remove_non_ascii_characters()
+		{
+			// Arrange
+			var urlSqueezer = new PathSqueezer();
+			string title = "विकी";
+
+			// Act
+			string actualPath = urlSqueezer.Squeeze(title);
+
+			// Assert
+			string expectedPath = "Děkujemeविकीвики-движка";
+
+			Assert.That(actualPath, Is.EqualTo(title));
+		}
+
+		[Test]
+		public void should_replace_whitespace_with_dashes_and_clean_dashes_and_punctuation()
+		{ 
+			// Arrange
+			var urlSqueezer = new PathSqueezer();
+			string title = "this is my title - and some \t\t\t\t\n   clever; (piece) of text here: [ok].";
+
+			// Act
+			string actualPath = urlSqueezer.Squeeze(title);
+
+			// Assert
+			string expectedPath = "this-is-my-title-and-some-clever-piece-of-text-here-ok";
+
+			Assert.That(actualPath, Is.EqualTo(expectedPath));
+		}
+
+		[Test]
+		public void should_replace_reserved_characters()
+		{
+			// Arrange
+			var urlSqueezer = new PathSqueezer();
+			string title = "this is my title? and #firstworldproblems :* :sadface=true";
+
+			// Act
+			string actualPath = urlSqueezer.Squeeze(title);
+
+			// Assert
+			string expectedPath = "this-is-my-title-and-firstworldproblems-sadfacetrue";
+
+			Assert.That(actualPath, Is.EqualTo(expectedPath));
+		}
+	}
+}
---
+++ src/SeoUrlSausage/UrlSqueezer.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace SeoUrlSausage
+{
+	public class PathSqueezer
+	{
+		public string Squeeze(string title)
+		{
+			// Remove reserved chars - this reads a lot better than a regex
+			string[] reserved =
+			{
+				"-", ".", "_", "~", ":", "/", "?", "#", "[", "]",
+				"@", "!", "$", "&", "'", "(", ")", "*", "+", ",",
+				";", "=", "}", ";"
+			};
+			foreach (string token in reserved)
+			{
+				title = title.Replace(token, "");
+			}
+
+			// Remove multiple spaces
+			title = Regex.Replace(title, @"\s+", " ");
+			var builder = new StringBuilder();
+
+			// These checks looks convulated but are needed for Unicode characters that 
+			// aren't in the BMP (Basic Multilingual Plane)
+			foreach (char t in title)
+			{
+				if (!Char.IsPunctuation(t) && !Char.IsSeparator(t) && !Char.IsSymbol(t) && !Char.IsControl(t))
+				{
+					builder.Append(t);
+				}
+				else if (Char.IsWhiteSpace(t))
+				{
+					builder.Append("-");
+				}
+			}
+
+			return builder.ToString();
+		}
+	}
+}
